7ba674885c684c1f1216230ef72febfa8296f3e6
Svyatoslav <ssn.24@uni-dubna.ru>
2025-02-20 21:51:50 +0300

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,32 @@
+﻿namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string st = textBox1.Text;
+            label1.Text = "Привет " + st;
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
