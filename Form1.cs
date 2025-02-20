ae74945a8934c8d3053db32b88c0b623c6e60b37
Svyatoslav <ssn.24@uni-dubna.ru>
2025-02-20 22:24:47 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,53 @@
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox1.Text);
+            label1.Text = Convert.ToString(num1 * num2);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox1.Text);
+            label1.Text = Convert.ToString(num1 + num2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox1.Text);
+            label1.Text = Convert.ToString(num1 - num2);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(textBox1.Text);
+            int num2 = Convert.ToInt32(textBox1.Text);
+            label1.Text = Convert.ToString(num1 / num2);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
